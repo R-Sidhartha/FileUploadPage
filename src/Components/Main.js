5f8477be5fadbbe5ca67aee71202399e2d4e28e4
R-Sidhartha <rsidhartha20@iitk.ac.in>
2023-11-04 01:07:30 +0530

    changes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,6 +3,7 @@ import { useDropzone } from "react-dropzone";
 import uploadpng from "./uploadpng2.png";
 import filepng from "./filepng.png";
 import { Link } from "react-router-dom";
+import './fileupload.css'
 
 
 const Main = ({ isMobileView }) => {
@@ -63,38 +64,42 @@ const Main = ({ isMobileView }) => {
 
   return (
     <>
-      <div className="fileupload">
-        <h2 className={`text-4xl mt-5  ${isMobileView ? "" : "ml-44 mb-16"}`}>
+      <div className={`fileupload ${isMobileView ? 'mobilefileupload':''}`}>
+        <h2 className={`text-4xl p-5 ${isMobileView ? "" : " mb-2"}`}>
           Upload Your File Here
         </h2>
+        <div className={`flex justify-center items-center h-1 ${isMobileView ? "" : ""}`}>
+        <div className="w-1/6 bg-gray-600 h-1/2 my-2 rounded-2xl opacity-70"></div>
+        </div>
+        <h4 className={`${isMobileView?'mb-1':'mb-8'} my-3 mx-1`}><span className="text-2xl">&#187;</span> Train Your Chatbot: Upload and Enhance its Knowledge and Skills with Our Convenient Upload Page.</h4>
         <div className={`${isMobileView ? "flex-col" : ""}flex w-screen`}>
           <div className={`${isMobileView ? "w-full" : "w-1/5 mt-16"}`}>
             <nav
-              className={`bg-gray-200 border-r border-gray-300  w-full flex justify-center items-center flex-col`}
+              className={`bg-transparent  w-full flex justify-center items-center flex-col`}
             >
               <ul
-                className={`p-4  my-5 ${isMobileView ? "flex" : "space-y-8"}`}
+                className={`p-4   ${isMobileView ? "flex" : "space-y-8"}`}
               >
-                <li className="flex items-center text-lg hover:opacity-60 mx-2">
-                  <i className="fa-regular fa-file text-gray-500 fa-lg"></i>
+                <li className="flex items-center text-xl hover:opacity-60 mx-2">
+                  <i className="fa-regular fa-file  fa-lg"></i>
                   <Link className="ml-2" to="/">
                     File
                   </Link>
                 </li>
-                <li className="flex items-center text-lg hover:opacity-60 mx-2">
-                  <i className="fa-regular fa-file-word text-gray-500 fa-lg"></i>
+                <li className="flex items-center text-xl hover:opacity-60 mx-2">
+                  <i className="fa-regular fa-file-word  fa-lg"></i>
                   <Link className="ml-2" to="/">
                     Text
                   </Link>
                 </li>
-                <li className="flex items-center text-lg hover:opacity-60 mx-2">
-                  <i className="fa-solid fa-earth-asia text-gray-500 fa-lg"></i>
+                <li className="flex items-center text-xl hover:opacity-60 mx-2">
+                  <i className="fa-solid fa-earth-asia  fa-lg"></i>
                   <Link className="ml-2" to="/">
                     Website
                   </Link>
                 </li>
-                <li className="flex items-center text-lg hover:opacity-60 mx-2">
-                  <i className="fa-regular fa-comments text-gray-500 fa-lg"></i>
+                <li className="flex items-center text-xl hover:opacity-60 mx-2">
+                  <i className="fa-regular fa-comments  fa-lg"></i>
                   <Link to="/" className="ml-2">
                     Q&A
                   </Link>
@@ -104,15 +109,15 @@ const Main = ({ isMobileView }) => {
           </div>
           <div
             className={`flex flex-col items-center  ${
-              isMobileView ? "mx-5 w-6/7" : "ml-36 w-1/2"
+              isMobileView ? "mx-5 w-6/7" : "ml-20 w-1/2"
             }`}
           >
-            <div className="border-dashed border-2 border-cyan-800 uploadbox p-4 bg-white  shadow-lg mx-10 w-full h-96 relative cursor-pointer" {...getRootProps()}>
+            <div className="rounded-2xl border-2 border-opacity-40 border-gray-600 uploadbox p-4 bg-gradient-to-b from-gray-400 via-gray-300 to-gray-200  shadow-lg mx-10 w-full h-96 relative cursor-pointer" {...getRootProps()}>
               <div className="flex flex-col items-center justify-center mt-14">
                 <div class="relative w-16 h-16 rounded-full">
-                  <div class="animate-ping border-2 border-cyan-300 rounded-full absolute inset-0 m-auto"></div>
-                  <div class="animate-ping border-2 border-cyan-400 rounded-full absolute inset-0 m-auto animate-delay-200"></div>
-                  <div class="animate-ping border-2 border-cyan-600 rounded-full absolute inset-0 m-auto animate-delay-600"></div>
+                <div class="w-10 h-10 border-4 border-gray-400 rounded-full absolute inset-0 m-auto"></div>
+                  <div class="border-dotted animate-ping2 border-2 border-cyan-600 rounded-full absolute inset-0 m-auto delay-100"></div>
+                  <div class="border-dotted animate-ping border-2 border-cyan-600 rounded-full absolute inset-0 m-auto delay-300 "></div>
 
                   <img class="w-20 relative z-50" src={uploadpng} alt="" />
                 </div>
@@ -144,7 +149,7 @@ const Main = ({ isMobileView }) => {
                   (Total Files: {files.length})
                 </span>
               </h2>
-              <div className="flex flex-col justify-center items-center mt-10">
+              <div className="flex flex-col justify-center items-center mt-2">
                 <ul className="mt-2 grid gap-4 grid-cols-5 ">
                   {files.map((file, index) => (
                     <li key={index} className="mb-2 mx-5">
@@ -184,12 +189,14 @@ const Main = ({ isMobileView }) => {
           </div>
           <div>
             {files.length > 0 && (
-              <div className={"mt-4 mx-10 flex flex-col"}>
+              <div className={`mt-4 mx-10 flex flex-col justify-center items-center `}>
                 <h2 className="text-xl font-semibold my-2">Total Characters</h2>
                 <p>{totalCharacters} characters</p>
+                <div>
                 <button className="btn block mt-7 px-4 py-2 rounded-md bg-gradient-to-r from-blue-300 via-purple-500 to-blue-200 text-lg font-semibold hover:opacity-90 my-2">
-                  Create Chatbot
+                  Create Chatbot &#8230;
                 </button>
+                </div>
               </div>
             )}
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+      ping: {
+        '0%':{transform: 'scale(1)',
+        opacity: '0.6' },
+        '50%': { transform: 'scale(1.5)',
+        opacity: '0.3' },
+        ' 100%': { transform: 'scale(2)',
+          opacity: '0' },
+      },
+      ping2: {
+        '0%':{transform: 'scale(1)',
+        opacity: '0.6' },
+        '25%': { transform: 'scale(1.2)',
+        opacity: '0.3' },
+        ' 100%': { transform: 'scale(2)',
+          opacity: '0' },
+      },
+    },
+    animation: {
+      ping: 'ping 1s ease-in-out infinite forwards',
+    },
+    animation: {
+      ping2: 'ping2 1s ease-in-out infinite forwards',
+    }
+  },
   },
   plugins: [],
 }
