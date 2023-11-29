4a2b8d0be9c2665264c02b3375cb1b4551e23507
R-Sidhartha <rsidhartha20@iitk.ac.in>
2023-11-30 01:20:20 +0530

    latest changes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -11,7 +11,7 @@ const Main = ({ isMobileView }) => {
   const [files, setFiles] = useState([]);
   const [totalCharacters, setTotalCharacters] = useState(0);
   const [completedUploads, setCompletedUploads] = useState([]);
-
+  
   // Function to handle text extraction for PDF files
   const extractPdfText = async (file) => {
     const loadingTask = pdfjs.getDocument(URL.createObjectURL(file));
@@ -107,14 +107,15 @@ const Main = ({ isMobileView }) => {
       <div
         className={`fileupload mt-16 ${isMobileView ? "mobilefileupload" : ""}`}
       >
-        <h2 className={`text-2xl font-bold p-4`}>Data Sources</h2>
+        <h2 className={`text-2xl font-bold p-4 text-center`}>Data Sources</h2>
         <div
           className={`flex justify-center items-center h-1 ${
             isMobileView ? "" : ""
           }`}
         ></div>
-        <h4 className={`${isMobileView ? "mb-1" : "mb-6"} my-1 mx-1 text-sm`}>
-          Train Your Chatbot: Upload and Enhance its Knowledge and Skills with Our Convenient Upload Page.
+        <h4 className={`${isMobileView ? "mb-1" : "mb-6"} my-1 mx-1 text-sm text-center`}>
+          Train Your Chatbot: Upload and Enhance its Knowledge and Skills with
+          Our Convenient Upload Page.
         </h4>
         <div
           className={`${
@@ -122,11 +123,19 @@ const Main = ({ isMobileView }) => {
           }flex flex-col w-screen  justify-center items-center`}
         >
           <div className="flex justify-center w-full">
-            <div className={`${isMobileView ? "w-full" : "w-1/5"} flex flex-col items-center mx-2 `}>
+            <div
+              className={`${
+                isMobileView ? "w-full" : "w-1/5"
+              } flex flex-col items-center mx-2 `}
+            >
               <nav
                 className={`bg-transparent  w-full flex justify-center items-center flex-col`}
               >
-                <ul className={`p-4 text-lg ${isMobileView ? "flex" : "space-y-4"}`}>
+                <ul
+                  className={`p-4 text-lg ${
+                    isMobileView ? "flex" : "space-y-4"
+                  }`}
+                >
                   <li className="flex items-center hover:opacity-60 mx-2">
                     <i className="fa-regular fa-file"></i>
                     <Link className="ml-2" to="/">
@@ -160,7 +169,7 @@ const Main = ({ isMobileView }) => {
               }`}
             >
               <div
-                className="rounded-2xl border-2 border-opacity-40 border-gray-500 uploadbox p-4 bg-gradient-to-b from-gray-100 via-white to-gray-100  shadow-lg mx-10 w-full h-96 relative cursor-pointer"
+                className="rounded-2xl border-2 border-opacity-40 border-gray-500 uploadbox p-4 bg-gradient-to-b from-gray-100 via-white to-gray-100  shadow-lg mx-10 w-full h-96 relative cursor-pointer flex flex-col items-center"
                 {...getRootProps()}
               >
                 <div className="flex flex-col items-center justify-center mt-14">
@@ -171,9 +180,9 @@ const Main = ({ isMobileView }) => {
 
                     <img class="w-20 relative z-50" src={uploadpng} alt="" />
                   </div>
-                  <h4 className=" mt-8 mb-16">
+                  <h4 className=" mt-8 mb-16 text-center">
                     Drag and drop your file here to upload
-                    <h6 className="text-sm opacity-70 my-2">
+                    <h6 className="text-sm opacity-70 my-2 text-center">
                       Supported file types: .pdf, .docs, .docx, .txt
                       (maxSize-15MB)
                     </h6>
@@ -186,25 +195,34 @@ const Main = ({ isMobileView }) => {
                 />
                 <label
                   htmlFor="file-upload"
-                  className=" w-full bg-gradient-to-r from-blue-300 via-purple-500 to-blue-200 p-3  rounded-md cursor-pointer hover:opacity-80 font-semibold "
+                  className=" w-1/5 bg-gradient-to-r from-blue-300 via-purple-500 to-blue-200 p-3  rounded-md cursor-pointer hover:opacity-80 font-semibold text-center"
                 >
                   Select Files
                 </label>
-                <div className="border-animation"></div>
               </div>
             </div>
 
-            <div className={`w-1/5 flex flex-col items-center ${files.length>0?'border border-gray-400 border-opacity-40 rounded-3xl':''} mx-2 `}>
+            <div
+              className={`w-1/5 flex flex-col items-center ${
+                files.length > 0
+                  ? "border border-gray-400 border-opacity-40 rounded-3xl"
+                  : ""
+              } mx-2 `}
+            >
               {files.length > 0 && (
                 <div
                   className={`mt-4 mx-10 flex flex-col justify-center items-center `}
                 >
                   <h2 className="text-lg font-semibold my-2">
-                    Total Characters <span className=" text-sm">
-                (Total Files: {files.length})
-              </span>
+                    Total Characters{" "}
+                    <span className=" text-sm">
+                      (Total Files: {files.length})
+                    </span>
                   </h2>
-                  <p className="text-sm">{totalCharacters} characters <span className="opacity-60">/10,00,000 limit</span></p>
+                  <p className="text-sm">
+                    {totalCharacters} characters{" "}
+                    <span className="opacity-60">/10,00,000 limit</span>
+                  </p>
                   <div className="w-full">
                     <button className="btn block mt-4 px-4 py-2 rounded-md bg-gradient-to-r from-blue-300 via-purple-500 to-blue-200  font-semibold hover:opacity-90 my-2 w-full text-lg">
                       Create Chatbot &#8230;
@@ -215,47 +233,51 @@ const Main = ({ isMobileView }) => {
             </div>
           </div>
           <div className="mt-4 w-11/12">
-            <h2 className="text-xl font-semibold mt-5">
-              Uploaded Files{" "}
-            </h2>
-            {files.length>0 ? ( 
-            <div className="flex flex-col justify-center items-center mt-2">
-              <ul className="mt-2 grid gap-4 grid-cols-3">
-                {files.map((file, index) => (
-                  <li key={index} className="mb-2 mx-5">
-                    <div className="flex flex-col items-center relative">
-                      <button
-                        onClick={() => removeFile(file.file)}
-                        className="relative bottom-0 left-40 opacity-60 hover:opacity-80"
-                      >
-                        <i class="fa-solid fa-times"></i>
-                      </button>
-                      <a
-                        href={URL.createObjectURL(file.file)}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="cursor-pointer flex items-center hover:opacity-80"
-                      >
-                        <img className="w-6 mx-1" src={filepng} alt="" />
-                        <span className="text-xs break-words w-11/12 text-center">
-                          {file.file.name} {file.file.size / 1024} KB
-                        </span>
-                        <span className="text-xs my-2"></span>
-                      </a>
-                      {file.progress < 100 &&
-                        !completedUploads.includes(file.file) && (
-                          <div className="w-64 bg-gradient-to-r from-blue-500 to-purple-500 rounded-full h-2 ml-2">
-                            <div
-                              className={`h-2 bg-gradient-to-r from-blue-500 to-purple-500 rounded-full w-${file.progress}`}
-                            ></div>
-                          </div>
-                        )}
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ):(<h2 className="my-3 opacity-70">No Files selected to Display</h2>)}
+            <h2 className="text-xl font-semibold mt-5 text-center">Uploaded Files </h2>
+            {files.length > 0 ? (
+              <div className="flex flex-col justify-center items-center mt-2">
+                <ul className="mt-2 grid grid-cols-3 w-11/12 ">
+                  {files.map((file, index) => (
+                    <li key={index} className="mb-2 mx-5 ">
+                      <div className="flex flex-col items-center ">
+                        <div className="flex w-full">
+                          <a
+                            href={URL.createObjectURL(file.file)}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={`cursor-pointer flex items-center hover:opacity-70 break-words w-3/4 `}
+                          >
+                            <img className="w-6 mx-1" src={filepng} alt="" />
+                            <span className={`text-xs break-words w-11/12 ml-1`}>
+                              {file.file.name} {file.file.size / 1024} KB
+                            </span>
+                          </a>
+                          <span className="text-xs my-2">
+                            {" "}
+                            <button className="ml-8 hover:opacity-60 opacity-80">
+                              <i
+                                onClick={() => removeFile(file.file)}
+                                className="fa-solid fa-trash text-black "
+                              ></i>
+                            </button>
+                          </span>
+                        </div>
+                        {file.progress < 100 &&
+                          !completedUploads.includes(file.file) && (
+                            <div className="w-64 bg-gradient-to-r from-blue-500 to-purple-500 rounded-full h-2 ml-2">
+                              <div
+                                className={`h-2 bg-gradient-to-r from-blue-500 to-purple-500 rounded-full w-${file.progress}`}
+                              ></div>
+                            </div>
+                          )}
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : (
+              <h2 className="my-3 opacity-70 text-center">No Files Uploaded</h2>
+            )}
           </div>
         </div>
       </div>
