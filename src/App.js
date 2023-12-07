41852622094936c0c517963aa13ac4c17760c1ec
R-Sidhartha <rsidhartha20@iitk.ac.in>
2023-12-08 03:14:17 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Route,BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Main from './Components/Main';
 import { useEffect, useState } from 'react';
-import { TotalCharactersProvider } from './Context/TotalCharactersContext';
 
 function App() {
   const [isMobileView, setIsMobileView] = useState(false);
@@ -28,11 +27,9 @@ function App() {
   return (
     <Router>
     <div className="App">
-    <TotalCharactersProvider>
       <Routes>
       <Route exact path="/"  element={<Main isMobileView={isMobileView} setTotalCharacters={setTotalCharacters} totalCharacters={totalCharacters}/>}/>
       </Routes>
-      </TotalCharactersProvider>
     </div>
     </Router>
   );
